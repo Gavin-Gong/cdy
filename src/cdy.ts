3c23fb0b4583c3840eb7a0fc548ba240eefde047
Gavin Gong <kefengong@gmail.com>
2020-04-03 22:33:46 +0800

    Basic

--- src/cdy.ts
+++ src/cdy.ts
@@ -1,8 +1,14 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
-// ...
-export default class DummyClass {
-  constructor() {
-    console.log('x')
-  }
+import sample from "lodash-es/sample";
+import { Hans } from "./data/zh";
+
+export default (text: string, seed = 0.3): string => {
+  return Array.from(text).map(v => {
+    if (Math.random() <= seed) {
+      return Hans[v] ? sample(Hans[v]) : v;
+    } else {
+      return v;
+    }
+  }).join("");
 }
--- test/cdy.test.ts
+++ test/cdy.test.ts
@@ -1,14 +1,7 @@
-import DummyClass from "../src/cdy"
+import cdy from "../src/cdy"
 
-/**
- * Dummy test
- */
 describe("Dummy test", () => {
   it("works if true is truthy", () => {
     expect(true).toBeTruthy()
-  })
-
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
   })
 })
